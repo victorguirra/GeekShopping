14c2ce4cc4c2255cbd93464e30dcede630501733
victorguirra <victor.hugo.guirra@gmail.com>
2024-01-02 15:39:55 -0300

    create identity server db context

---
+++ GeekShopping.IdentityServer/Model/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(){ }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+    }
+}
