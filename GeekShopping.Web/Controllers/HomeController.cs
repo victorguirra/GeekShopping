3f678da9b11b38097911ce20e0e04d5ed031511f
victorguirra <victor.hugo.guirra@gmail.com>
2024-01-03 16:32:24 -0300

    redirect to login page

--- GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping.Web/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace GeekShopping.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public async Task<IActionResult> Login()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
     }
 }
