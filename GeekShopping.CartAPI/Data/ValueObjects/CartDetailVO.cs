6b3c88fb3b65a66644277ae0b23d073bf93d872f
victorguirra <victor.hugo.guirra@gmail.com>
2024-01-05 00:49:12 -0300

    create value objects based on entities

---
+++ GeekShopping.CartAPI/Data/ValueObjects/CartDetailVO.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.CartAPI.Data.ValueObjects
+{
+    public class CartDetailVO
+    {
+        public Int64 Id { get; set; }
+        public Int64 CartHeaderId { get; set; }
+        public CartHeaderVO CartHeader { get; set; }
+        public Int64 ProductId { get; set; }
+        public ProductVO Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ GeekShopping.CartAPI/Data/ValueObjects/CartHeaderVO.cs
@@ -0,0 +1,9 @@
+﻿namespace GeekShopping.CartAPI.Data.ValueObjects
+{
+    public class CartHeaderVO
+    {
+        public Int64 Id { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
